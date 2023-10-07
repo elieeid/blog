f17a0a18c6a884430c37205cd31df704f0b2db97
EID Elie <eeid@softwaymedical.fr>
2023-10-07 11:39:05 +0200

    Init blog elieeid with docusaurus2

---
+++ blog/2023-10-05-pourquoi-ce-blog/index.md
@@ -0,0 +1,8 @@
+---
+slug: welcome
+title: Pourquoi ce blog ?
+---
+
+[Docusaurus blogging features](https://docusaurus.io/docs/blog) are powered by the [blog plugin](https://docusaurus.io/docs/api/plugins/@docusaurus/plugin-content-blog).
+
+
---
+++ docusaurus.config.js
@@ -0,0 +1,86 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Elie Eid',
+  tagline: 'Respect my authority',
+  favicon: 'img/cartman-respect-my-authority.webp',
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-test-site.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'elieeid', // Usually your GitHub org/user name.
+  projectName: 'elieeid', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'fr',
+    locales: ['fr'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: false,
+        blog: {
+          routeBasePath: '/', // Serve the blog at the site's root
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/elieeid/elieeid',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      navbar: {
+        title: 'Elie Eid',
+        logo: {
+          alt: 'Cartman',
+          src: 'img/cartman-respect-my-authority.webp',
+        },
+        items: [
+          {
+            to: '/about',
+            label: 'A propos',
+            position: 'right',
+          },
+          {
+            href: 'https://github.com/elieeid/elieeid',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
